59f2a10d4359cbdb4daefada2736e6ca6e7242a8
junkluis <lufezuro@gmail.com>
2018-07-29 23:02:55 -0500

    testing more

--- api/tests.py
+++ api/tests.py
@@ -9,7 +9,7 @@ from django.test import TestCase
 from .models import *
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.conf import settings
 from django.contrib.auth.models import User

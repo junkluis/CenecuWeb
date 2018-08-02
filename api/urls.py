be5efba9efb6c4f559bd019bbe2ccdc3b4859924
junkluis <lufezuro@gmail.com>
2018-08-02 08:49:30 -0500

    jwt

--- api/urls.py
+++ api/urls.py
@@ -19,6 +19,7 @@ from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 
 
+
 urlpatterns = {
     url(r'^$', views.api_index, name="Index"),
     url(r'^Horario/$', HorarioView.as_view(), name="Horario"),
@@ -72,7 +73,11 @@ urlpatterns = {
     url(r'^borrarNota/(?P<pk>[0-9]+)/$', views.borrar_nota, name="borrar_nota"),
     url(r'^registrarUsuario/$', views.registrar_usuario,
         name="registrar_usuario"),
-    url(r'^logOut/$', views.log_out, name="log_out")
+    url(r'^logOut/$', views.log_out, name="log_out"),
+
+    url(r'^auth-jwt/', obtain_jwt_token),
+    url(r'^auth-jwt-refresh/', refresh_jwt_token),
+    url(r'^auth-jwt-verify/', verify_jwt_token),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- cenecu_web/settings.py
+++ cenecu_web/settings.py
@@ -73,6 +73,23 @@ TEMPLATES = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+JWT_AUTH = { 
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300)
+}
+
+
 WSGI_APPLICATION = 'cenecu_web.wsgi.application'
 
 

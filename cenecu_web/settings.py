f3fb0bf5d7cd59ae92e6a4c99dac230bdd0dd788
junkluis <lufezuro@gmail.com>
2018-08-05 21:51:57 +0000

    merge leafs on migrations

--- cenecu_web/settings.py
+++ cenecu_web/settings.py
@@ -84,9 +84,10 @@ REST_FRAMEWORK = {
     ),
 }
 
-JWT_AUTH = { 
+JWT_AUTH = {
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300)
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30)
 }
 
 
